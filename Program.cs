7157cc3bcc703d33fb75da00c6dd2e9c338581f1
Jean-Dias <jeandias0803@gmail.com>
2025-05-07 10:52:13 -0300

    adição dos metodos get, post, put, delete.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using trabalho;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configura o banco SQLite
+builder.Services.AddDbContext<EstoqueContext>(options =>
+    options.UseSqlite("Data Source=produtos.db"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// GET - Todos os produtos
+app.MapGet("/produtos", async (EstoqueContext db) =>
+{
+    try
+    {
+        var produtos = await db.Produtos.ToListAsync();
+        return Results.Ok(produtos);
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Erro ao buscar produtos: {ex.Message}");
+    }
+});
+
+// GET - Produto por ID
+app.MapGet("/produtos/{id}", async (int id, EstoqueContext db) =>
+{
+    try
+    {
+        var produto = await db.Produtos.FindAsync(id);
+        return produto != null ? Results.Ok(produto) : Results.NotFound();
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem($"Erro ao buscar produto: {ex.Message}");
+    }
+});
+
+app.MapPost ("produtos", async (Produto produto, EstoqueContext db) => 
+{
+try 
+{
+    // adiciona um produto na tabelo protudos no banco de dados
+    db.Produtos.Add(produto);
+
+    // faz o sistema esperar salvar no banco de dados
+    await db.SaveChangesAsync();
+    // retorna status 200 ( ok ) e mostra o produto adicionado e id
+    return Results.Ok($"/produtos/{produto.Id}");
+}catch(Exception ex)
+{
+    // retorna uma mensagem de erro junto  caso nao seja possivel criar o produto e retorna status 201 (Created)
+return Results.Problem($"Erro, problema ao criar produto: {ex.Message}", statusCode: 500);
+}
+});
+
+app.MapDelete ("/produtos/{id}", async (int id, EstoqueContext db) => 
+{
+    try 
+    {
+        var produto = await db.Produtos.FindAsync(id);
+        if (produto == null )
+        return Results.NotFound();
+
+        db.Produtos.Remove(produto);
+        await db.SaveChangesAsync();
+        return Results.Ok ($"Produto com id {id} removido com sucesso");
+    }catch(Exception ex)
+    {
+        return Results.Problem($"Erro ao remover o produto: {ex.Message}", statusCode: 500);
+    }
+} );
+
 app.Run();
--- obj/Debug/net8.0/trabalho.AssemblyInfo.cs
+++ obj/Debug/net8.0/trabalho.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("trabalho")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8cb6e1cd3eb86f9d1726d690aeb36f1d8004e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca2c6dd1e04ee62b968ba3374e7c6f92cc2d1ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("trabalho")]
 [assembly: System.Reflection.AssemblyTitleAttribute("trabalho")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
